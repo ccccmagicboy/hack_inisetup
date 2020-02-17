658140d4889d727cefdd0534afb0f45c554a5e26
cccc <cuiwei_cv@163.com>
2020-02-17 19:11:52 +0800

    add set cpu freq at initial #3

--- action.py
+++ action.py
@@ -1,10 +1,22 @@
+#import machine
+#machine.freq(240*1000*1000)
+import fileinput
 import os
-str = os.environ['INPUT_SLOT']
-print('input slot is {0:s}'.format(str))
+import sys
 
+def replaceAll(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
+        
+str = os.environ['INPUT_CPU']
+print('set cpu to {0:s}MHz.'.format(str))
 print(os.listdir(os.getcwd()))
 
-str = '0000'
+mk_path = os.path.join(os.getcwd(), 'my_micropython', 'ports', 'esp32', 'modules', 'inisetup.py')
+print(mk_path)
+replaceAll(mk_path, "#webrepl.start()","import machine;machine.freq(240*1000*1000);#webrepl.start()")
 
 if None != str:
     command = 'echo ::set-output name=result::{0:s}'.format(str)
