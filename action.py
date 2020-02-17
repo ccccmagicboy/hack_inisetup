45ae6cac4cd06f758f4b3cd6867b9a049f19f0fb
cccc <cuiwei_cv@163.com>
2020-02-17 19:37:20 +0800

    set freq works! and check the freq user input

--- action.py
+++ action.py
@@ -10,16 +10,23 @@ def replaceAll(file,searchExp,replaceExp):
             line = line.replace(searchExp,replaceExp)
         sys.stdout.write(line)
         
+#frequency must be 20MHz, 40MHz, 80Mhz, 160MHz or 240MHz
+freq_list = ['20', '40', '80', '160', '240']
 str = os.environ['INPUT_CPU']
-print('set cpu to {0:s}MHz.'.format(str))
-print(os.listdir(os.getcwd()))
 
-mk_path = os.path.join(os.getcwd(), 'my_micropython', 'ports', 'esp32', 'modules', 'inisetup.py')
-print(mk_path)
-replaceAll(mk_path, "#webrepl.start()","import machine;machine.freq(240*1000*1000);#webrepl.start()")
+if str in freq_list:
+    print('set cpu to {0:s}MHz.'.format(str))
+    print(os.listdir(os.getcwd()))
+    mk_path = os.path.join(os.getcwd(), 'my_micropython', 'ports', 'esp32', 'modules', 'inisetup.py')
+    print(mk_path)
+    replaceAll(mk_path, "#webrepl.start()","import machine;machine.freq({0:s}*1000*1000);#webrepl.start()".format(str))
+    result = 'good!'
+else:
+    result = 'bad!'
+    print('frequency must be 20MHz, 40MHz, 80Mhz, 160MHz or 240MHz')
 
 if None != str:
-    command = 'echo ::set-output name=result::{0:s}'.format(str)
+    command = 'echo ::set-output name=result::{0:s}'.format(result)
     print(command)
     print(os.popen(command).read())
 
