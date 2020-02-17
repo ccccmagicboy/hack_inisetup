de42ee8829e6d1f8ca26e2838521c775d611cf08
cccc <cuiwei_cv@163.com>
2020-02-17 19:54:55 +0800

    set initial freq for esp8266

--- action.py
+++ action.py
@@ -1,13 +1,32 @@
+#import machine
+#machine.freq(160*1000*1000)
+import fileinput
 import os
-str = os.environ['INPUT_SLOT']
-print('input slot is {0:s}'.format(str))
+import sys
 
-print(os.listdir(os.getcwd()))
+def replaceAll(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
+        
+#frequency can only be either 80Mhz or 160MHz
+freq_list = ['80', '160']
+str = os.environ['INPUT_CPU']
 
-str = '0000'
+if str in freq_list:
+    print('set cpu to {0:s}MHz.'.format(str))
+    print(os.listdir(os.getcwd()))
+    mk_path = os.path.join(os.getcwd(), 'my_micropython', 'ports', 'esp8266', 'modules', 'inisetup.py')
+    print(mk_path)
+    replaceAll(mk_path, "#webrepl.start()","import machine;machine.freq({0:s}*1000*1000);#webrepl.start()".format(str))
+    result = 'good!'
+else:
+    result = 'bad!'
+    print('frequency can only be either 80Mhz or 160MHz')
 
 if None != str:
-    command = 'echo ::set-output name=result::{0:s}'.format(str)
+    command = 'echo ::set-output name=result::{0:s}'.format(result)
     print(command)
     print(os.popen(command).read())
 
